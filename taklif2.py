2f73e93733fe4f8242d683fa853ab7263210501d
imansedgh <iman.sedgh@yahoo.com>
2019-09-12 00:51:33 +0430

    everything ok,needs to change input to raw input

---
+++ taklif2.py
@@ -0,0 +1,27 @@
+print 'started'
+
+firstnumber = 1
+OddNumber = 0
+while firstnumber%2 != 0:
+        print 'Please Enter an Even (ZOj)  Number'
+        firstnumber = input()
+     
+#print 'Your Number Is ',firstnumber
+counter = firstnumber
+while counter > 0 :
+    
+    
+    while OddNumber%2 !=1 :  
+        print firstnumber-(counter-1),': Now Enter an Odd (Fard) Number'    
+        OddNumber = input()
+    #neveshtan adad va kam kardan
+    while OddNumber >= 1 :
+        print 'Odd Number :',OddNumber
+        OddNumber -= 2
+    OddNumber = 0
+    counter -= 1    
+#    print 'another loop'
+
+print 'End of the program'    
+    
+    
